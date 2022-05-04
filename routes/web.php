95952aef5099e8797c1526af0e582de24f0b23a5
Hagai Raja Sinulingga <hagairaja@gmail.com>
2022-05-05 01:20:07 +0900

    add basic scanner

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ScannerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [ScannerController::class, 'index'])->name('scanner.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
